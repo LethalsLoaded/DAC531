22d16f9a5301a95827b1a49053d9d4ac5b77e2df
Keron320 <keron320@gmail.com>
2019-02-10 19:28:23 +0000

    menuscene

---
+++ Assets/Scripts/MenuScripts/rotationScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationScript : MonoBehaviour
+{
+    public float speed = 5; // Default speed is 5
+
+    void Update()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+
+    }
+}
