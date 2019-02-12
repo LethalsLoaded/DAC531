a236bec536dc36b4410efe41bbeb4eede8b0292f
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-02-12 13:31:46 +0000

    Weather, nighttime and room E setup
    + Night Skybox
    + Nighttime ambient light
    + Rain audio and particles
    + Thunder Audio
    ~ Glass material chaned to tranparent

---
+++ Assets/Scripts/WeatherScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AzureSky;
+
+public class WeatherScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AzureSkyController skyController; 
+    public int minSecond;
+    public int maxSeconds;
+    void Start()
+    {
+        skyController.SetNewWeatherProfile(1);
+        StartCoroutine(Thunder());
+    }    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("switch to indoor weather");
+            skyController.SetNewWeatherProfile(2);
+        }
+    }
+    IEnumerator Thunder()
+    {
+        yield return new WaitForSeconds(Random.Range(minSecond,maxSeconds));
+        skyController.PlayThunderEffectRandom();
+        yield return new WaitForSeconds(5);
+        StartCoroutine(Thunder());
+    }
+    
+}
