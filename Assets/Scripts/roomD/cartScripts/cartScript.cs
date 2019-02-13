33f346cd6346d280d964116490f981d07872f412
Keron320 <keron320@gmail.com>
2019-02-13 15:37:42 +0000

    Cart WIP

---
+++ Assets/Scripts/roomD/cartScripts/cartScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cartScript : MonoBehaviour
+{
+    Rigidbody mRigidbody;
+    public float moveSpeed = 1;
+    public GameObject myCart;
+
+    bool moveCart;
+
+    private void Update()
+    {
+        if (moveCart == true)
+            myCart.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && other.tag != "Object") 
+        {
+            moveCart = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            moveCart = false;
+        }
+    }
+}
