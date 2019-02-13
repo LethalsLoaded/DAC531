0faf892563d2d41a6e7cdbdbefe112240cc62d08
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-02-13 13:39:49 +0000

    Finished room E design and reimport of skybox assets
    + classroom assets
    + black candle

---
+++ Assets/Scripts/CandleLightScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleLightScript : MonoBehaviour
+{
+    public float MaxReduction;
+     public float MaxIncrease;
+     public float RateDamping;
+     public float Strength;
+     public bool StopFlickering;
+ 
+     private Light lightSource;
+     private float intensity;
+     private bool flickering;
+ 
+ 
+     public void Start()
+     {
+         lightSource = GetComponent<Light>();
+
+         intensity = lightSource.intensity;
+         StartCoroutine(DoFlicker());
+     }
+ 
+     void Update()
+     {
+         if (!StopFlickering && !flickering)
+         {
+             StartCoroutine(DoFlicker());
+         }
+     }
+ 
+      IEnumerator DoFlicker()
+     {
+         flickering = true;
+         while (!StopFlickering)
+         {
+             lightSource.intensity = Mathf.Lerp(lightSource.intensity, Random.Range(intensity - MaxReduction, intensity + MaxIncrease), Strength * Time.deltaTime);
+             yield return new WaitForSeconds(RateDamping);
+         }
+         flickering = false;
+     }
+}
