7febcb36cbf6610f696e7c1dc7feb31b0723a180
Keron320 <keron320@gmail.com>
2019-02-17 14:25:53 +0000

    Andrews latest update ( Dialog stuff)
    because I messed something up

--- Assets/Scripts/roomD/eventTrigger.cs
+++ Assets/Scripts/roomD/eventTrigger.cs
@@ -1,51 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class eventTrigger : MonoBehaviour
 {
     //Add event that plays
     public GameObject myEvent;
-    public GameObject myPlayer;
-
-    public float eventTimer;
-    // event has not occured
-    // Idea would be to make this script modular
-    private bool fadeEvent = false;
-
-
-    IEnumerator fadeTimer()
-    {
-        //Wait 1 second
-        yield return new WaitForSeconds(eventTimer);
-        myEvent.GetComponent<Animator>().enabled = false;
-        myEvent.GetComponent<Image>().enabled = false;
-
-        //Allows player movement again
-        myPlayer.GetComponent<PlayerController>().enabled = true;
-
-        //We no longer require the event
-        Destroy(gameObject);
-    }
+    public GameObject myEvent2;
 
     private void OnTriggerEnter(Collider other)
     {
-        #region fadeEvent
-        if (other.tag == "Player" && fadeEvent == false)
+        if (other.tag == "Player")
         {
-            myEvent.GetComponent<Animator>().enabled = true;
-            myEvent.GetComponent<Image>().enabled = true;
-
-            //Find the player and disable controls
-            myPlayer.GetComponent<PlayerController>().enabled = false;
-
-            //fadeEvent happened
-            fadeEvent = true;
-            //Starts coroutine
-            StartCoroutine(fadeTimer());
-
+            
         }
-        #endregion
     }
 }
