90819bf0e1cf46ce71ac1d3dc7666588dcc1041f
Keron320 <keron320@gmail.com>
2019-02-20 21:09:47 +0000

    Room stuff
    Added
    +As you walk through you hear a little nagging voice telling you that it seems somehow familiar, maybe you’ve been here before.
    
    +You close your eyes allowing your subconscious to guide you. You open eyes and find something in your hands.

---
+++ Assets/Scripts/roomD/singleEventTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class singleEventTrigger : MonoBehaviour
+{
+    //Add event that plays
+    public GameObject myEvent;
+
+    public float eventTimer;
+
+
+
+    IEnumerator fadeTimer()
+    {
+        //Wait 1 second
+        yield return new WaitForSeconds(eventTimer);
+    }
+    private void Start()
+    {
+        myEvent.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        #region fadeEvent
+        if (other.tag == "Player")
+        {
+            Debug.Log("Hi");
+            myEvent.SetActive(true);
+        }
+        #endregion
+    }
+}
