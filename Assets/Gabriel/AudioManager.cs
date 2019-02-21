5c3a43d4c346e578b188caec07cfb5778abc8c05
gabrielpotamianos <33257116+gabrielpotamianos@users.noreply.github.com>
2019-02-21 15:46:27 +0000

    Room A/F and Animations + instructions

---
+++ Assets/Gabriel/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+[System.Serializable]
+public class AudioManager : MonoBehaviour {
+
+    public Sounds[] sounds;
+    // Use this for initialization
+    private void Awake()
+    {
+        foreach(Sounds s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Array.Find(sounds, sounds => sounds.name == name).source.Play();
+        
+    }
+}
