b355c86ef1e2305c7a9f58a6ecf496baad139e2c
Keron320 <keron320@gmail.com>
2019-02-24 21:13:22 +0000

    HUD stuff

---
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+// Functions that can be called from anywhere in the main menu
+public class MenuManager : MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
