6bbb655452c88f7ef03f591ad171a3bdad199a47
Keron320 <keron320@gmail.com>
2019-02-25 22:01:13 +0000

    Credits button on main menu
    works now.

---
+++ Assets/Scripts/setActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActive : MonoBehaviour
+{
+    public GameObject activateObject;
+    public GameObject disableObject;
+
+    public void enableObject()
+    {
+        activateObject.SetActive(true);
+        disableObject.SetActive(false);
+    }
+
+
+}
