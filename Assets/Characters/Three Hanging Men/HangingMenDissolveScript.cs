44d02b2d7462949dc8cc45789b0227e88e710e4e
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-03-01 14:28:19 +0000

    second floor cinematics

---
+++ Assets/Characters/Three Hanging Men/HangingMenDissolveScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangingMenDissolveScript : MonoBehaviour
+{
+   public ParticleSystem[] particles;
+   public Material[] material;
+    bool isDissolving = false;
+       public float speed;
+        
+
+        void OnTriggerEnter(Collider col)
+        {
+                if (col.transform.tag == "Player")
+                isDissolving = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isDissolving)
+            {
+                //Animating cutout
+                foreach (var matts in material)
+                {
+                    matts.SetFloat("_DissolveCutoff", speed * Time.time);
+                }
+                foreach (var item in particles)
+                {
+                    item.gameObject.SetActive(true);
+                }
+            }
+        }
+}
