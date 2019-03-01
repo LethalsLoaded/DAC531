71ac7d2c069870ec4b6dd1d44b354e86d6ccc196
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-03-01 11:55:51 +0000

    First Flor Timeline Activation Fixes

---
+++ Assets/Cinemachine/TimelineActivationScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineActivationScript : MonoBehaviour
+{
+    public GameObject timeline;
+    public float disableInSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            timeline.SetActive(true);
+            StartCoroutine(Timeline());
+        }
+    }
+
+    IEnumerator Timeline()
+    {
+        yield return new WaitForSeconds(disableInSeconds);
+        timeline.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+}
